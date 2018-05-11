62d25f7d0558e0db44d241e73d8ad89caa696b4c
Adrian Davis <chuck.d.kc@gmail.com>
2018-05-11 19:05:06 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(speak)
-  if speak == speak.upcase
+  if speak.upcase == speak
     puts "NO, NOT SINCE 1938!"
 =begin 
   elsif speak == "I LOVE YOU GRANDMA!"
